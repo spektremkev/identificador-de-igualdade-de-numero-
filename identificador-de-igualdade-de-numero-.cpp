4ec7227ce6f4e7b07758ef4ca90c1002a88824fd
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2020-10-04 22:15:01 -0300

    Update identificador-de-igualdade-de-numero-.cpp

--- identificador-de-igualdade-de-numero-.cpp
+++ identificador-de-igualdade-de-numero-.cpp
@@ -3,12 +3,8 @@
 #include <locale.h>
 
 
-/*Nomes:Izabelly Azevedo Lima - Matricola 2019201251
-		Gabriel dos SN Martis - Matricola 2019200071
-		Raquel Vieira Cardoso - Matricola 2019200854
-		*/
-		
- //Faça um programa em C que leia um número e verifique se este é igual a 100, menor que 100, maior que 100. 
+
+ //FaÃ§a um programa em C que leia um nÃºmero e verifique se este Ã© igual a 100, menor que 100, maior que 100. 
  //codigo feito no dia 05/05/2020
  
 int main(void)<%
